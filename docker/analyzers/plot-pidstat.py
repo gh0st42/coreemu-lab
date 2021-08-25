a4f33688e27ca7d78346081c6a7ae42516ce07aa
gh0st <1264131+gh0st42@users.noreply.github.com>
2021-08-25 11:39:23 +0200

    changed mpl backend to Agg

--- docker/analyzers/plot-pidstat.py
+++ docker/analyzers/plot-pidstat.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import fnmatch
--- docker/analyzers/plot-xy.py
+++ docker/analyzers/plot-xy.py
@@ -2,8 +2,9 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 import os
 import fnmatch
 import re
